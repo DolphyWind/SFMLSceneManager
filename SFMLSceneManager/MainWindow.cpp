1917383498edeb54f49ce558ef2f7583daca0512
BuildTools <unconfigured@null.spigotmc.org>
2022-08-21 01:12:12 +0300

    Added an example to MainWindow class

--- SFMLSceneManager/MainWindow.cpp
+++ SFMLSceneManager/MainWindow.cpp
@@ -18,11 +18,18 @@ void MainWindow::processEvents()
 
 void MainWindow::update(sf::Time deltaTime)
 {
+	// Change scenes based on input
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+		m_sceneManager.setActiveScene(m_scene1.getName());
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+		m_sceneManager.setActiveScene(m_scene2.getName());
 }
 
 void MainWindow::draw()
 {
 	m_window.clear(sf::Color::Black);
+
+	m_sceneManager.draw();
 
 	m_window.display();
 }
@@ -31,6 +38,25 @@ MainWindow::MainWindow()
 {
 	m_window.create(sf::VideoMode(m_windowWidth, m_windowHeight), "Window", sf::Style::Close);
 	m_window.setFramerateLimit(FPS);
+
+	m_sceneManager.setParentWindow(&m_window);
+
+	// Create Scenes
+	m_redSquare.setSize(sf::Vector2f(50, 50));
+	m_redSquare.setFillColor(sf::Color::Red);
+	m_scene1.addSceneObject(&m_redSquare);
+	m_scene1.setName("Red Square");
+
+	m_blueSquare.setSize(sf::Vector2f(50, 50));
+	m_blueSquare.setPosition(sf::Vector2f(50, 50));
+	m_blueSquare.setFillColor(sf::Color::Blue);
+	m_scene2.addSceneObject(&m_blueSquare);
+	m_scene2.setName("Blue Square");
+
+	// Add scenes to scene manager
+	m_sceneManager.addScene(&m_scene1);
+	m_sceneManager.addScene(&m_scene2);
+	m_sceneManager.setActiveScene(m_scene1.getName());
 }
 
 void MainWindow::mainLoop()
--- SFMLSceneManager/MainWindow.h
+++ SFMLSceneManager/MainWindow.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "SceneManager.h"
+#include "Scene.h"
 
 class MainWindow
 {
@@ -10,6 +12,14 @@ private:
 	const sf::Time m_timePerFrame = sf::seconds(1.f / (float)FPS);
 	unsigned int m_windowWidth = 800;
 	unsigned int m_windowHeight = 600;
+
+	SceneManager m_sceneManager;
+
+	Scene m_scene1;
+	sf::RectangleShape m_redSquare;
+
+	Scene m_scene2;
+	sf::RectangleShape m_blueSquare;
 
 	void processEvents();
 	void update(sf::Time deltaTime);
